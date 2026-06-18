45121cb86e03883aa9fe85670321a4f02f2e01e3
bolshakov <Bolshakov@lanit.ru>
2026-06-18 14:02:34 +0300

    fix (config): Добавлена библиотека requests и еще переменную в конфиг

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ load_dotenv()  # Загружает все переменные из .env
 TEG_USERNAME_TG_BOT = os.getenv('TEG_USERNAME_TG_BOT')
 TOKEN_ACCESS_API_TG_BOT = os.getenv('TOKEN_ACCESS_API_TG_BOT')
 API_CORE_TG = os.getenv('API_CORE_TG')
+API_URL_TG = os.getenv('API_URL_TG')
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+BASE_URL = f'{API_URL_TG}{TOKEN_ACCESS_API_TG_BOT}'
